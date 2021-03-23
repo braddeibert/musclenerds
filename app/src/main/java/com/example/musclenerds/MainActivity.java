981c3ab6f7b2971826b8b3b2fb5a16b90bf8be4f
robDevs <robbdevv@gmail.com>
2021-03-23 16:28:19 -0600

    example usage of database

--- app/src/main/java/com/example/musclenerds/MainActivity.java
+++ app/src/main/java/com/example/musclenerds/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.musclenerds;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.musclenerds.database.AppDatabase;
+import com.example.musclenerds.database.AppExecutors;
+import com.example.musclenerds.database.MotivationalQuoteDAO;
+import com.example.musclenerds.model.MotivationalQuote;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -16,9 +21,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private AppDatabase mDb; // make a reference to the database.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        mDb = AppDatabase.getInstance(getApplicationContext()); // let the database handle creating a singleton instance. Must be done in an activity or something that extends an activity.
+
+        //then use the executor to handle the execution.
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                MotivationalQuote newQuote = new MotivationalQuote("Android dev is fun", "Rob"); //create a quote.
+                mDb.quoteDao().insertQuote(newQuote); // insert a quote.
+                List<MotivationalQuote> quotes = mDb.quoteDao().getAllQuotes(); //get all quotes.
+                Log.d("AppData_LOG", "Fetched " + quotes.size() + " quotes from database: '");
+                //log all the quotes.
+                for(int i = 0; i < quotes.size(); i++) {
+                    Log.d("AppData_LOG", "Id: " + quotes.get(i).getId() + "; text: " + quotes.get(i).getText() + "; author: " + quotes.get(i).getAuthor());
+                }
+            }
+        });
     }
 
     @Override
