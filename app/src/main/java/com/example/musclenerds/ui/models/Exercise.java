92753ae038b643d79c0a3d0b6f88a85f079e79cc
JosiahHamer2 <jh104485@umconnect.umt.edu>
2021-03-28 13:41:15 -0600

    Adding RecyclerView, exercise_view fragment, adapters

---
+++ app/src/main/java/com/example/musclenerds/ui/models/Exercise.java
@@ -0,0 +1,8 @@
+package com.example.musclenerds.ui.models;
+
+//The model I was using to create objects, will need to be swapped with the new database models.
+public class Exercise {
+    public int id;
+    public String exerciseName;
+    public String imageUrl;
+}
