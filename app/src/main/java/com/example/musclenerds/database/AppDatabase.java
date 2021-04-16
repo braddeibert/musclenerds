25acee521c49bfe894bb63d3d976f5422799e563
robDevs <robbdevv@gmail.com>
2021-04-16 13:46:32 -0600

    exercise-equipment table

--- app/src/main/java/com/example/musclenerds/database/AppDatabase.java
+++ app/src/main/java/com/example/musclenerds/database/AppDatabase.java
@@ -9,6 +9,7 @@ import androidx.room.RoomDatabase;
 
 import com.example.musclenerds.model.Equipment;
 import com.example.musclenerds.model.Exercise;
+import com.example.musclenerds.model.ExerciseEquipment;
 import com.example.musclenerds.model.MotivationalQuote;
 import com.example.musclenerds.model.Muscle;
 import com.example.musclenerds.model.MuscleGroup;
@@ -24,7 +25,7 @@ import com.example.musclenerds.model.WorkoutExercise;
 //See below for brief explanation on the singleton pattern.
 
 //this tells ROOm that this is a database, what entities it has, etc.
-@Database(entities = {MotivationalQuote.class, Exercise.class, TrackedSet.class, TrackedWorkout.class, Workout.class, WorkoutExercise.class, Muscle.class, Equipment.class, MuscleGroup.class, MuscleGroups.class}, version = 2, exportSchema = false)
+@Database(entities = {MotivationalQuote.class, Exercise.class, TrackedSet.class, TrackedWorkout.class, Workout.class, WorkoutExercise.class, Muscle.class, Equipment.class, MuscleGroup.class, MuscleGroups.class, ExerciseEquipment.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static final String LOG_TAG = "AppData_LOG"; // name used in debug log.
     private static final Object LOCK = new Object(); // not sure tbh.
@@ -61,4 +62,5 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract EquipmentDAO equipmentDAO();
     public abstract MuscleGroupDAO muscleGroupDAO();
     public abstract MuscleGroupsDAO muscleGroupsDAO();
+    public abstract ExerciseEquipmentDAO exerciseEquipmentDAO();
 }
