35b08a8452a92d3cca749df4732df3e10e3ccf1c
robDevs <robbdevv@gmail.com>
2021-04-23 12:22:51 -0600

    fix typo

--- app/src/main/java/com/example/musclenerds/WorkoutHistory.java
+++ app/src/main/java/com/example/musclenerds/WorkoutHistory.java
@@ -195,7 +195,6 @@ public class WorkoutHistory extends Fragment {
             public void run() {
                 int year = 2021;
                 int month = 3;
-                List<TrackedWorkout> allWorkouts = mDb.trackedWorkoutDAO().getAll()
                     for(int day = 1; day < 30; day++) {
                         Date currDate = new Date(year, month, day);
                         long currTime = currDate.getTime();
