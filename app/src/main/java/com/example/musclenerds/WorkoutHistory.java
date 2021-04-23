fe126474ddc613cbc5ff7ed72b988e9bffa894e2
robDevs <robbdevv@gmail.com>
2021-04-23 00:09:25 -0600

    create onSelectedDayChange()

--- app/src/main/java/com/example/musclenerds/WorkoutHistory.java
+++ app/src/main/java/com/example/musclenerds/WorkoutHistory.java
@@ -4,11 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.musclenerds.database.AppDatabase;
+import com.example.musclenerds.ui.home.HomeViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +33,9 @@ public class WorkoutHistory extends Fragment {
     //  arameters
     private String mParam1;
     private String mParam2;
+
+    private HomeViewModel homeViewModel;
+    private AppDatabase mDb;
 
     public WorkoutHistory() {
         // Required empty public constructor
@@ -67,13 +77,27 @@ public class WorkoutHistory extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mDb = AppDatabase.getInstance(getContext());
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_workout_history, container, false);
+
+        CalendarView calendarView=(CalendarView) root.findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                Log.d("cal_log", "time: " + dayOfMonth);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout_history, container, false);
+        return root;
     }
 }
