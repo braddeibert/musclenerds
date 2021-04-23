9072d862904557595adc53eea2f42da8beab60a1
robDevs <robbdevv@gmail.com>
2021-04-23 11:51:37 -0600

    cleanup code and view, populate random tracked workouts for april if none found, populate workout highlight on creating history view

--- app/src/main/java/com/example/musclenerds/WorkoutHistory.java
+++ app/src/main/java/com/example/musclenerds/WorkoutHistory.java
@@ -1,6 +1,5 @@
 package com.example.musclenerds;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,14 +12,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.musclenerds.database.AppDatabase;
 import com.example.musclenerds.database.AppExecutors;
 import com.example.musclenerds.model.Exercise;
-import com.example.musclenerds.model.MotivationalQuote;
 import com.example.musclenerds.model.TrackedWorkout;
 import com.example.musclenerds.model.Workout;
 import com.example.musclenerds.model.WorkoutExercise;
@@ -30,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -101,58 +98,115 @@ public class WorkoutHistory extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_workout_history, container, false);
 
+        setRandomWorkouts();
+
+        //grab the calendarView object
         CalendarView calendarView=(CalendarView) root.findViewById(R.id.calendarView);
+        //the text view that will display the workout highlight on the ui
+        TextView workoutView = root.findViewById(R.id.WorkoutTextView);
+
+        Date currDate = new Date(Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        long currTime = currDate.getTime();
+        String currtimeString = String.valueOf(currTime);
+        Log.d("cal_log", "time: " + Calendar.getInstance().get(Calendar.YEAR) + "/" + Calendar.getInstance().get(Calendar.MONTH) + "/" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + ":" + currTime);
+
+        //need to get the grab the current day info on creating the view.
+        //a listener that we add after this chunk will handle grabbing info on
+        //selected day being changed.
+        setWorkoutHighlight(currtimeString, root);
+
+        //attach a listener to the calendarView object.
+        //this will fire when the selected date is changed.
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month,
                                             int dayOfMonth) {
-                Log.d("cal_log", "time: " + dayOfMonth);
+                //create a date object based on the selected yea, day, month
                 Date date = new Date(year, month, dayOfMonth);
 
-                Calendar converter = Calendar.getInstance();
-                converter.set(year, month, dayOfMonth);
+                //get the time sends epoch
                 long timeStamp = date.getTime();
 
-                date = new Date(year, month, dayOfMonth);
-                long testTimeStamp = date.getTime();
-
-                Log.d("cal_log", "time: " + timeStamp);
-                Log.d("cal_log", "test time: " + testTimeStamp);
+                //store it in a string.
                 String time = String.valueOf(timeStamp);
-                String testTime = String.valueOf(testTimeStamp);
-                Log.d("cal_log", "time: " + time);
+                Log.d("cal_log", "time: " + month);
 
-                AppExecutors.getInstance().diskIO().execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        List<TrackedWorkout> allDayWorkouts = mDb.trackedWorkoutDAO().findByDate(testTime);
-                        if(allDayWorkouts.size() < 1) {
-                            TrackedWorkout testworkout = new TrackedWorkout(1, 20, String.valueOf(testTimeStamp));
-                            mDb.trackedWorkoutDAO().insert(testworkout);
-                        }
-                        allDayWorkouts = mDb.trackedWorkoutDAO().findByDate(time);
-                        Log.d("cal_log", "size: " + allDayWorkouts.size());
+                setWorkoutHighlight(time, root);
 
-                        Log.d("cal_log", "workouts: " + allDayWorkouts.get(0).getDateCompleted());
-                        TextView workoutView = root.findViewById(R.id.WorkoutTextView);
-
-                        Workout curr_work = mDb.workoutDAO().findById(allDayWorkouts.get(0).getW_ID());
-
-                        final String workoutName = curr_work.getName() + "\n" + curr_work.getDescription();
-
-                        new Handler(Looper.getMainLooper()).post(new Runnable(){
-                            @Override
-                            public void run() {
-                                workoutView.setText(workoutName);
-
-                            }
-                        });
-                    }
-                });
             }
         });
         // Inflate the layout for this fragment
         return root;
+    }
+
+    public void setWorkoutHighlight(String time, View root) {
+        TextView workoutView = root.findViewById(R.id.WorkoutTextView);
+        //create a thread for db ops
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                //string to store the workout info
+                String workoutHighlight;
+                //the list of workouts for a given day.
+                List<TrackedWorkout> allDayWorkouts = mDb.trackedWorkoutDAO().findByDate(time);
+                //if the workouts are empty, show a message instead of a workout.
+                if(allDayWorkouts.size() < 1) {
+                    workoutHighlight = "Nothing to show for this day, get to work!!";
+                }
+                //else, the list should be a list of workouts for the given day, ordered by most recent.
+                //grab the first one on the list and show it's info.
+                else {
+                    Workout curr_work = mDb.workoutDAO().findById(allDayWorkouts.get(0).getW_ID());
+
+                    workoutHighlight = curr_work.getName() + "\n" + curr_work.getDescription();
+
+                    List<WorkoutExercise> workoutExercises = mDb.workoutExerciseDAO().findByW_ID(curr_work.getId());
+
+                    //then make a list of all the exercises from the workoutExercises list.
+                    List<Exercise> exercises = new ArrayList<Exercise>();
+                    for(int i = 0; i < workoutExercises.size(); i++) {
+                        exercises.add(mDb.exerciseDAO().findById(workoutExercises.get(i).getE_ID()));
+                        workoutHighlight += "\n-" + mDb.exerciseDAO().findById(workoutExercises.get(i).getE_ID()).getName();
+                        Log.d("cal_log", mDb.exerciseDAO().findById(workoutExercises.get(i).getE_ID()).getName());
+                    }
+                }
+
+                //textView.seText() expects a final string.
+                final String uiWorkoutHighlight = workoutHighlight;
+
+                //create a thread to set the text of the textView object.
+                new Handler(Looper.getMainLooper()).post(new Runnable(){
+                    @Override
+                    public void run() {
+                        workoutView.setText(uiWorkoutHighlight);
+
+                    }
+                });
+            }
+        });
+    }
+
+    public void setRandomWorkouts() {
+        //create a thread for db ops
+        Random rand = new Random();
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                int year = 2021;
+                int month = 3;
+                List<TrackedWorkout> allWorkouts = mDb.trackedWorkoutDAO().getAll();
+                if(allWorkouts.size() < 1)
+                    for(int day = 1; day < 30; day++) {
+                        Date currDate = new Date(year, month, day);
+                        long currTime = currDate.getTime();
+                        String timeString = String.valueOf(currTime);
+                        int workout = rand.nextInt(3) + 1;
+                        Log.d("cal_log", "rand" + workout);
+                        TrackedWorkout newTrackedWorkout = new TrackedWorkout(workout, 30, timeString);
+                        mDb.trackedWorkoutDAO().insert(newTrackedWorkout);
+                    }
+            }
+        });
     }
 }
